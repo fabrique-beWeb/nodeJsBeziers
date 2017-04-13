3664a361f1e9e8e1007559e72cbd41e1a9d4a97b
LoicDerrieux <l.derrieux@fondespierre.com>
2017-04-13 12:32:14 +0200

    mise en place du FW express

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
-var http = require('http');
+var express = require('express');
 
-var ip = '127.0.0.1';
-var port = 12107;
+var serveur = express();
+var routeToto = "/toto";
 
-var server = http.createServer(
-	function(request,response){
-		response.statusCode = 200;
-		response.setHeader('Content-Type','text/plain');
-		response.end('allez vous faire foutre les marseillais/parisiens');
-		
-	}
-
-);
-
-server.listen(port,ip,function(){
-	console.log('le serveur tourne aussi bien que le psg');
+serveur.get("/",function(request,response){
+	response.sendFile(__dirname + "/views/index.html");
 });
+serveur.get(routeToto,function(request,response){
+	response.send("message venant de toto");
+});
+serveur.post("/toto",function(request,response){
+	response.send("message venant de toto en post");
+});
+serveur.listen(12107);
